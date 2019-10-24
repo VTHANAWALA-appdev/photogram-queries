b8ddc6cd84d283477f0b166903470cf76517047a
vthanawala <vthanawa@chicagobooth.edu>
2019-10-24 20:24:19 +0000

    Classwork completed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -13,4 +13,28 @@
 #
 
 class Photo < ApplicationRecord
+    def poster
+        return User.where({:id => self.owner_id}).at(0)
+    end
+    def comments
+        return Comment.where({:photo_id => self.id})
+    end
+    def likes
+        return Like.where({:photo_id => self.id})
+    end
+    def fans
+        y =  Like.where({:photo_id => self.id}).pluck(:fan_id)
+        return User.where({:id => y})
+    end
+    def fan_list
+        y =  Like.where({:photo_id => self.id}).pluck(:fan_id)
+        users = User.where({:id => y}).pluck(:username)
+        list = String.new
+        p numberusers = users.count
+        return numberusers
+        numberusers.times do |times|
+            p list = list + users(times)
+        end
+        return list
+    end
 end
